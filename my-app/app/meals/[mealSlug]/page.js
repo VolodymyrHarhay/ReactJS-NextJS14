7e577f733e282d26484b07dbc47fba8d07d64993
VolodymyrHarhay <volodymyrharhay@gmail.com>
2024-03-30 18:09:35 -0500

    added image picker and share a meal with saving logic

--- my-app/app/meals/[mealSlug]/page.js
+++ my-app/app/meals/[mealSlug]/page.js
@@ -1,11 +1,41 @@
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
-const SelectedMeal = ({params}) => {
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+  console.log({meal})
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
   return (
     <>
-      This is Selected Meal - 
-      {params.mealSlug}
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={`/images/${meal.image}`} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
     </>
   );
-};
-
-export default SelectedMeal;
+}
